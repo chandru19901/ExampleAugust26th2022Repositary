05cdf9a7968ddf3937288c544c6410153c2bb692
chandru19901 <117055266+chandru19901@users.noreply.github.com>
2022-11-14 13:01:51 +0530

    Add files via upload

---
+++ TestNG-Automation/src/com/Assign1.java
@@ -0,0 +1,172 @@
+package com.TestNGActitime.programs;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Assign1 {
+
+	public static WebDriver oBrowser=null;
+	@Test(priority = 1)
+	public static void launchBrowser()
+	{
+		try
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 2)
+	public static void navigate()
+	{
+		WebElement oEle=null;
+		try
+		{
+			oBrowser.get("http://localhost/login.do");
+			Thread.sleep(5000);
+			oEle=oBrowser.findElement(By.xpath("//td[text()='Please identify yourself']"));
+			Assert.assertTrue(oEle.isDisplayed());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 3,dataProvider = "login")
+	public static void login(String user,String pwd)
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys(user);
+			oBrowser.findElement(By.name("pwd")).sendKeys(pwd);
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(5000);
+			WebElement oLink=oBrowser.findElement(By.xpath("//a[contains(text(),'Administrator')]"));
+			Assert.assertTrue(oLink.isDisplayed());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 4)
+	public static void minimizeFlyOutwindow()
+	{
+		String expected;
+		try
+		{
+			expected="Getting Started Shortcuts";
+			WebElement oEle=oBrowser.findElement(By.xpath("//div[text()='Getting Started Shortcuts']"));
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+			String actual=oEle.getText();
+			Assert.assertEquals(expected, actual);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority=5, dataProvider="createUser")
+	public static void createUser(String fn, String mn,String ln, String eml,String un, String pw, String pwc)
+	{
+		WebElement oEle=null;
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr[1]/td[5]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[text()='Add User']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.name("firstName")).sendKeys(fn);
+			oBrowser.findElement(By.name("middleName")).sendKeys(mn);
+			oBrowser.findElement(By.name("lastName")).sendKeys(ln);
+			oBrowser.findElement(By.name("email")).sendKeys(eml);
+			oBrowser.findElement(By.name("username")).sendKeys(un);
+			oBrowser.findElement(By.name("password")).sendKeys(pw);
+			oBrowser.findElement(By.name("passwordCopy")).sendKeys(pwc);
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//span[text()='Create User']")).click();
+			Thread.sleep(5000);
+			oEle= oBrowser.findElement(By.xpath("//span[text()='3 accounts left']"));
+			Assert.assertTrue(oEle.isDisplayed());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 6)
+	public static void deleteUser()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//span[text()='User1, demo A.']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.id("userDataLightBox_deleteBtn")).click();
+			Thread.sleep(2000);
+			Alert oAlert=oBrowser.switchTo().alert();
+			String content=oAlert.getText();
+			System.out.println(content);
+			oAlert.accept();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 7)
+	public static void logout()
+	{
+		String expected,actual;
+		try
+		{
+			expected="actiTIME - Login";
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+			actual=oBrowser.getTitle();
+			Assert.assertEquals(expected, actual);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 8)
+	public static void closeApp()
+	{
+		try
+		{
+			oBrowser.quit();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@DataProvider(name="login")
+	public Object[][] getLoginData()
+	{
+		return new Object[][] {{"admin","manager"}};
+	}
+
+	@DataProvider(name= "createUser")
+	public Object[][] getUserData()
+	{
+		return new Object[][] {{"demo","A","User1","demo@gmail.com","demoUser1","1234","1234"}};
+
+	}
+
+}
+
+
+
+
---
+++ TestNG-Automation/src/com/Assignment7.java
@@ -0,0 +1,214 @@
+package com.TestNGActitime.programs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Assignment7 {
+
+	public static WebDriver oBrowser=null;
+
+	@Test(priority = 1)
+	public static void launchBrowser()
+	{
+		try
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 2)
+	public static void navigate()
+	{
+		WebElement oEle=null;
+		try
+		{
+			oBrowser.get("http://localhost/login.do");
+			Thread.sleep(5000);
+			oEle=oBrowser.findElement(By.xpath("//td[text()='Please identify yourself']"));
+			Assert.assertTrue(oEle.isDisplayed());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 3,dataProvider = "login")
+	public static void login(String user,String pwd)
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys(user);
+			oBrowser.findElement(By.name("pwd")).sendKeys(pwd);
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(5000);
+			WebElement oLink=oBrowser.findElement(By.xpath("//a[contains(text(),'Administrator')]"));
+			Assert.assertTrue(oLink.isDisplayed());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 4)
+	public static void minimizeFlyOutwindow()
+	{
+		String expected;
+		try
+		{
+			expected="Getting Started Shortcuts";
+			WebElement oEle=oBrowser.findElement(By.xpath("//div[text()='Getting Started Shortcuts']"));
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+			String actual=oEle.getText();
+			Assert.assertEquals(expected, actual);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 5, dataProvider = "addCustomer")
+	public static void addCustomer(String cmr)
+	{                                                  
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr/td[3]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='Add New'] ")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[1]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("customerLightBox_nameField")).sendKeys(cmr);
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"customerLightBox_commitBtn\"]/div/span")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 6,dataProvider = "createProject")
+	public static void creatProject(String pjr)                                 
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[3]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='Add New'] ")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[2]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("projectPopup_projectNameField")).sendKeys(pjr);
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"projectPopup_commitBtn\"]/div/span")).click();
+			Thread.sleep(3000);
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 7)
+	public static void ModifyProject()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='cpTreeBlock']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[3]/div[3]")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//*[@id='taskListBlock']/div[4]/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/div[1]")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//div[text()='Archived']")).click();
+			Thread.sleep(1000);
+
+
+
+		}catch(Exception e)
+
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority =8 )
+	public static void DeleteProject()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='cpTreeBlock']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(2000);
+
+			oBrowser.findElement(By.xpath("//div[text()='ACTIONS']")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//div[text()='Delete']")).click();
+			Thread.sleep(1000);
+			oBrowser.findElement(By.xpath("//span[text()='Delete permanently']")).click();
+			Thread.sleep(1000);
+
+		}catch(Exception e)
+
+		{
+			e.printStackTrace();
+		}
+	}
+	@Test(priority = 9)
+	public static void logout()
+	{
+		String expected,actual;
+		try
+		{
+			expected="actiTIME - Login";
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+			actual=oBrowser.getTitle();
+			Assert.assertEquals(expected, actual);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 10)
+	public static void closeApp()
+	{
+		try
+		{
+			oBrowser.quit();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@DataProvider(name="login")
+	public Object[][] getLoginData()
+	{
+		return new Object[][] {{"admin","manager"}};
+	}
+
+
+
+	@DataProvider(name = "createProject")
+	public Object[][] addproject()
+	{
+		return new Object[][] {{"Gaganayana"}};
+
+	}
+	@DataProvider(name = "addCustomer")
+	public Object[][] addcustomer()
+	{
+		return new Object[][] {{"Akash"}};
+
+	}
+}
+
+
+
+
+
